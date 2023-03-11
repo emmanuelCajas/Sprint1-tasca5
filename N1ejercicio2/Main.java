3b99f8d5a94d270bef6cb19868d4e2787d2870c3
Emmanuel Cajas <emmanuelcajasballesteros@gmail.com>
2023-03-11 08:09:57 +0100

    Corrigiendo N1

--- N1ejercicio2/Main.java
+++ N1ejercicio2/Main.java
@@ -8,6 +8,7 @@ import java.util.Date;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		String nombre = "c:/Users/rodri/Desktop/meses";
 		String separador = "";
 		File directorio = new File(nombre);
--- N1ejercicio5/Main.java
+++ N1ejercicio5/Main.java
@@ -2,56 +2,73 @@
 package tasca5.N1ejercicio5;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 
 public class Main {
 
-	
-	
-	
-	public static void main(String[] args) {
+	public static void serializar(Producto[] listaProductos) {
+
+		ObjectOutputStream serializando;
+		String directorio = "C:/Users/rodri/Desktop/producto.ser";
+		try {
+			serializando = new ObjectOutputStream(new FileOutputStream(directorio));
+			serializando.writeObject(listaProductos);
+			serializando.close();
+
+			System.out.println("El archivo ha sido creado...");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void deSerializar(String directorio) {
 		
+		ObjectInputStream deserializando;
+		try {
+			deserializando = new ObjectInputStream(
+					new FileInputStream(directorio));
+			Producto[] listaProductosDeserlializado = (Producto[]) deserializando.readObject();
+
+			// Imprimiendo lista de Productos deserializados
+
+			for (Producto p : listaProductosDeserlializado) {
+				System.out.println(p);
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+
+	public static void main(String[] args) {
 
 		Producto producto1 = new Producto("ventilador", 45.00);
 		Producto producto2 = new Producto("monitor", 125.00);
-		
+
 		Producto[] listaProductos = new Producto[2];
-		
+
 		listaProductos[0] = producto1;
 		listaProductos[1] = producto2;
 		
+		serializar(listaProductos);
 		
-		try {
-			
-			//Serializando
-			
-			ObjectOutputStream serializando = new ObjectOutputStream(new FileOutputStream("C:/Users/rodri/Desktop/producto.ser"));
-			serializando.writeObject(listaProductos);
-			serializando.close();
-			
-			System.out.println("El archivo ha sido creado...");
-			
-			
-			//Deserializando
-			
-			ObjectInputStream deserializando = new ObjectInputStream(new FileInputStream("C:/Users/rodri/Desktop/producto.ser"));
-			Producto[] listaProductosDeserlializado =(Producto[]) deserializando.readObject();
-			
-			
-			
-			//Imprimiendo lista de Productos deserializados
-			
-			for (Producto p : listaProductosDeserlializado) {
-				System.out.println(p);		
-			}			
-			
-		} catch (Exception e) {
-			
-		}
-		
+		deSerializar("C:/Users/rodri/Desktop/producto.ser");
+
+
 	}
 
 }
