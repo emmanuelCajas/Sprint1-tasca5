314e5627ed3de33e896213d71f0dd705778e2a47
Emmanuel Cajas <emmanuelcajasballesteros@gmail.com>
2023-03-13 23:47:33 +0100

    corriegiendo ejercicio3

--- N1ejercicio3/Main.java
+++ N1ejercicio3/Main.java
@@ -5,34 +5,61 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
 
-	
+	private static List<String> datosArchivo = new ArrayList<>();
+
+	public static void listarDirectorio(String path, String separador) {
+		File d = new File(path);
+		String[] listaDirectorios = d.list();
+
+		for (String string : listaDirectorios) {
+			File archivo = new File(d, string);
+
+			long fecha = archivo.lastModified();
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm aa");
+			Date lastModifiedDate = new Date(fecha);
+
+			datosArchivo.add(separador + " " + string + "   " + (archivo.isDirectory() ? "D" : "F") + "   "
+					+ simpleDateFormat.format(lastModifiedDate));
+
+			System.out.println(separador + " " + string + "   " + (archivo.isDirectory() ? "D" : "F") + "   "
+					+ simpleDateFormat.format(lastModifiedDate));
+
+			if (archivo.isDirectory()) {
+				path = archivo.getPath();
+				listarDirectorio(path, separador + " ");
+			}
+		}
+	}
+
+	public void crearArchivoTxt(List<String> datosArchivo) {
+
+		File file = new File("C:/Users/rodri/Desktop/pruebas.txt");
+		try {
+			FileWriter fileWriter = new FileWriter(file);
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			for (String s : datosArchivo) {
+				bufferedWriter.write(s + "\n");
+			}
+			bufferedWriter.close();
+			System.out.println("Archivo creado");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
 
-		String dir = "C:/Users/rodri/Desktop/meses";
-		String dirArchivo = "C:/Users/rodri/Desktop/pruebas.txt";
-		File directorio = new File(dir);
-
-		BufferedWriter bw = null;
-
-		try {
-			bw = new BufferedWriter(new FileWriter(dirArchivo));
-			ListaDirectorio.listarDirectorios(directorio, "", bw);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (bw != null) {
-				try {
-					bw.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+		String path = args[0];
+		Main app = new Main();
+		app.listarDirectorio(path, "");
+		app.crearArchivoTxt(datosArchivo);
 
 	}
 
