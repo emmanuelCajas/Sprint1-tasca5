be899fdd166b2400dd0175d025b8a30a612c79f0
Emmanuel Cajas <emmanuelcajasballesteros@gmail.com>
2023-03-13 12:24:55 +0100

    corrigiendo N1ejercicio1

--- N1ejercicio1/Main.java
+++ N1ejercicio1/Main.java
@@ -1,10 +1,33 @@
 package tasca5.N1ejercicio1;
 
+import java.io.File;
+import java.util.Arrays;
 
 public class Main {
+	File file;
+	static String path;
+	
+	public  void listaDirectorio(String dir) {
+
+		file = new File(dir);
+		String[] listaDirectorios = file.list();
+
+		Arrays.sort(listaDirectorios);
+
+		if (file.isDirectory()) {
+			for (String listado : listaDirectorios) {
+				System.out.println(listado);
+			}
+		} else {
+			System.out.println("La ruta ingresada no es un directorio");
+		}
+	}
+
 	public static void main(String[] args) {
 		
-		ListaDirectorio.listaDirectorio("c:/Users/rodri/Desktop");
-
+	    path = args[0];
+		Main app = new Main();
+		app.listaDirectorio(path);
+		
 	}
 }
